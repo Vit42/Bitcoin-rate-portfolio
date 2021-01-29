d8221c24db28ae0acf4dc0be217d980200c0a81b
Vit 42 <v.v.safronov@gmail.com>
2021-01-29 18:26:37 +0100

    Initial Commit

---
+++ Bitcoin Rate (portfolio)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bitcoin Rate (portfolio)
+//
+//  Created by Vitalii Safronov on 2021. 01. 29..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
