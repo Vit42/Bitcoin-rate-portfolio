78d6d88133fdbd792c62e7be801dab1fc4d8e83b
Vit 42 <v.v.safronov@gmail.com>
2021-03-08 13:52:39 +0100

    initial commit

---
+++ Bitcoin Rate (portfolio)/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  Bitcoin Rate (portfolio)
+//
+//  Created by Vitalii Safronov on 2021. 01. 29..
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
